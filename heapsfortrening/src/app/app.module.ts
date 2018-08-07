43bdd9c72c6648b2fa628b6841c83a3674064043
viki-korotkaya <viki.korotkaya@gmail.com>
2018-08-07 13:45:49 -0400

    directive

--- heapsfortrening/src/app/app.module.ts
+++ heapsfortrening/src/app/app.module.ts
@@ -4,10 +4,11 @@ import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
 import {ChildComponent} from "./child.component";
 import {TemplateComponent} from "./template.component";
+import {BoldDirective} from "./bold.directive";
 
 @NgModule({
     imports: [BrowserModule, FormsModule],
-    declarations: [AppComponent, ChildComponent, TemplateComponent],
+    declarations: [AppComponent, ChildComponent, TemplateComponent, BoldDirective],
     bootstrap: [AppComponent]
 })
 
